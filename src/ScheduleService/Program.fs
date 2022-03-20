6f3d2d5b0b75ac323083501b43cd8bcceb2f4eda
nikitakaralyus <nikitakaralyus@gmail.com>
2022-03-20 16:07:34 +0400

    Created ScheduleService project

---
+++ src/ScheduleService/Program.fs
@@ -0,0 +1,28 @@
+namespace ScheduleService
+
+#nowarn "20"
+
+open Microsoft.AspNetCore.Builder
+open Microsoft.Extensions.DependencyInjection
+open Microsoft.Extensions.Hosting
+
+module Program =
+    let exitCode = 0
+
+    [<EntryPoint>]
+    let main args =
+
+        let builder = WebApplication.CreateBuilder(args)
+
+        builder.Services.AddControllers()
+
+        let app = builder.Build()
+
+        app.UseHttpsRedirection()
+
+        app.UseAuthorization()
+        app.MapControllers()
+
+        app.Run()
+
+        exitCode
