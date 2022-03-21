b2b9dc5f2bc02588ecf51166dc94ca6e4f6dc267
nikitakaralyus <nikitakaralyus@gmail.com>
2022-03-21 17:01:26 +0400

    Added Transport, Direction and Route types

--- src/ScheduleService/Types.fs
+++ src/ScheduleService/Types.fs
@@ -3,3 +3,14 @@ module WhereIsTheBus.ScheduleService.Types
 type Arrival = { StopId: int; TimeToArrive: int }
 
 type Stop = { Id: int; Name: string; TimeToArrive: int }
+
+type Transport =
+    | Bus
+    | Trolleybus
+    | Tram
+
+type Direction =
+    | Direct
+    | Return
+
+type Route = { Type: Transport; Number: int; Direction: Direction  }
