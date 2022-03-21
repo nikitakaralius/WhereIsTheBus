1bd1d6573b2c658ebe17eabf0004da59dfadb8e7
nikitakaralyus <nikitakaralyus@gmail.com>
2022-03-21 17:47:41 +0400

    Created RouteUrlBuilder

--- src/ScheduleService/RouteUrlBuilder.fs
+++ src/ScheduleService/RouteUrlBuilder.fs
@@ -3,8 +3,34 @@ module WhereIsTheBus.ScheduleService.RouteUrlBuilder
 open System
 open WhereIsTheBus.ScheduleService.Types
 
+let private host = "igis.ru"
+
+let private transport route =
+    match route.Transport with
+    | Bus -> "bus/izh"
+    | Trolleybus -> "trol"
+    | Tram -> "tram"
+
+let private mode route =
+    match route.Transport with
+    | Bus -> "1"
+    | Trolleybus -> "2"
+    | Tram -> "4"
+
 let stopsUrlOf route =
-    0
+    UriBuilder()
+        .UseHttps()
+        .WithHost(host)
+        .WithPathSegment("gortrans")
+        .WithPathSegment(route |> transport)
+        .WithPathSegment(route.Number |> string)
+        .Uri
 
 let arrivalsUrlOf route =
-    0
+    UriBuilder()
+        .UseHttps()
+        .WithHost(host)
+        .WithPathSegment("com/page/online.php")
+        .WithParameter("nom", route.Number |> string)
+        .WithParameter("mode", route |> mode)
+        .Uri
