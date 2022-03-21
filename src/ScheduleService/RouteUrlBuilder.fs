4f79ec424df0920ea8995a87c0f9427428568b69
nikitakaralyus <nikitakaralyus@gmail.com>
2022-03-21 17:32:48 +0400

    Imported Fluent Uri Builder package

---
+++ src/ScheduleService/RouteUrlBuilder.fs
@@ -0,0 +1,10 @@
+module WhereIsTheBus.ScheduleService.RouteUrlBuilder
+
+open System
+open WhereIsTheBus.ScheduleService.Types
+
+let stopsUrlOf route =
+    0
+
+let arrivalsUrlOf route =
+    0
--- src/ScheduleService/Types.fs
+++ src/ScheduleService/Types.fs
@@ -13,4 +13,4 @@ type Arrival = { StopId: int; TimeToArrive: int }
 
 type Stop = { Id: int; Name: string; TimeToArrive: int }
 
-type Route = { Type: Transport; Number: int; Direction: Direction  }
+type Route = { Type: Transport; Number: int; Direction: Direction }
