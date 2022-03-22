978de5a4b2b49b1846177ec8e46b67bc068258ac
nikitakaralyus <nikitakaralyus@gmail.com>
2022-03-22 21:02:43 +0400

    Turned RoutesDbFillTool into a console application

--- src/RoutesDbFillTool/Program.fs
+++ src/RoutesDbFillTool/Program.fs
@@ -1,20 +1,8 @@
 namespace WhereIsTheBus.RoutesDbFillTool
 
-#nowarn "20"
-
-open Microsoft.AspNetCore.Builder
-open Microsoft.Extensions.Hosting
-
 module Program =
     let exitCode = 0
 
     [<EntryPoint>]
     let main args =
-
-        let builder = WebApplication.CreateBuilder(args)
-            
-        let app = builder.Build()
-
-        app.Run()
-        
         exitCode
