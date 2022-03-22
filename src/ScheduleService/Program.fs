646d0c00e35e4f16ff1da39a0dee8e8e293ffe97
nikitakaralyus <nikitakaralyus@gmail.com>
2022-03-22 20:49:15 +0400

    Removed controllers

--- src/ScheduleService/Program.fs
+++ src/ScheduleService/Program.fs
@@ -2,9 +2,7 @@ namespace ScheduleService
 
 #nowarn "20"
 
-open System.Text.Json.Serialization
 open Microsoft.AspNetCore.Builder
-open Microsoft.Extensions.DependencyInjection
 open Microsoft.Extensions.Hosting
 
 module Program =
@@ -14,20 +12,8 @@ module Program =
     let main args =
 
         let builder = WebApplication.CreateBuilder(args)
-
-        builder.Services
-            .AddControllers()
-            .AddJsonOptions(fun options -> options.JsonSerializerOptions.Converters.Add(
-                JsonFSharpConverter(
-                    unionTagCaseInsensitive = true,
-                    unionEncoding = JsonUnionEncoding.UnwrapFieldlessTags)))
             
         let app = builder.Build()
-
-        app.UseHttpsRedirection()
-
-        app.UseAuthorization()
-        app.MapControllers()
 
         app.Run()
         
