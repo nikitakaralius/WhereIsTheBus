1a6f0311a37f4efe30a8dba0888b428213cb21b1
nikitakaralyus <nikitakaralyus@gmail.com>
2022-04-05 19:25:31 +0400

    Added error handling in Handle Update Service

--- src/TelegramBot/Telegram/HandleUpdateService.cs
+++ src/TelegramBot/Telegram/HandleUpdateService.cs
@@ -4,11 +4,15 @@ internal class HandleUpdateService : IHandleUpdateService
 {
     private readonly IMediator _mediator;
     private readonly ITelegramRouter _router;
+    private readonly ILogger<HandleUpdateService> _logger;
 
-    public HandleUpdateService(IMediator mediator, ITelegramRouter router)
+    public HandleUpdateService(IMediator mediator, 
+                               ITelegramRouter router,
+                               ILogger<HandleUpdateService> logger)
     {
         _mediator = mediator;
         _router = router;
+        _logger = logger;
     }
 
     public async Task EchoAsync(Update update)
@@ -24,7 +28,14 @@ internal class HandleUpdateService : IHandleUpdateService
         {
             return;
         }
-        
-        await _mediator.Send(query);
+
+        try
+        {
+            await _mediator.Send(query);
+        }
+        catch (Exception e)
+        {
+            _logger.LogError("Handle error {errorMessage}", e.Message);
+        }
     }
 }
