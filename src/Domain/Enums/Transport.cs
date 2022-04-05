83c310a5476599d8b41cfbae2fb3ee42b3246233
nikitakaralyus <nikitakaralyus@gmail.com>
2022-04-06 00:34:47 +0400

    Extracted Domain layer

---
+++ src/Domain/Enums/Transport.cs
@@ -0,0 +1,9 @@
+namespace WhereIsTheBus.Domain.Enums;
+
+public enum Transport
+{
+    None = 0,
+    Bus = 1,
+    Trolleybus = 2,
+    Tram = 3
+}
---
+++ src/Domain/Records/TransportStop.cs
@@ -0,0 +1,16 @@
+namespace WhereIsTheBus.Domain.Records;
+
+public record TransportStop(int Id, string Name, StrictDirection Direction, int TimeToArrive)
+{
+    public virtual bool Equals(TransportStop? other)
+    {
+        if (ReferenceEquals(null, other)) return false;
+        if (ReferenceEquals(this, other)) return true;
+        return Id == other.Id;
+    }
+
+    public override int GetHashCode()
+    {
+        return Id;
+    }
+}
