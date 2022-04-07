54d74bbe2800c90d1d36a317305982efb21e80b7
DimaUraz <102070914+DimaUraz@users.noreply.github.com>
2022-04-07 19:05:23 +0400

    Added TransportQueryHandler

--- src/TelegramBot/Handlers/TransportQueryHandler.cs
+++ src/TelegramBot/Handlers/TransportQueryHandler.cs
@@ -1,3 +1,5 @@
+using Telegram.Bot.Types.ReplyMarkups;
+
 namespace WhereIsTheBus.TelegramBot.Handlers;
 
 public class TransportQueryHandler : IRequestHandler<TransportQuery>
@@ -11,6 +13,15 @@ public class TransportQueryHandler : IRequestHandler<TransportQuery>
 
     public async Task<Unit> Handle(TransportQuery request, CancellationToken cancellationToken)
     {
-        
+        InlineKeyboardMarkup keyboard = new(new[]
+        {
+            InlineKeyboardButton.WithCallbackData("29", "/bus 29"), 
+            InlineKeyboardButton.WithCallbackData("79", "/bus 79"), 
+            InlineKeyboardButton.WithCallbackData("28", "/bus 28"), 
+        });
+        await _telegram.SendTextMessageAsync(request.Update.ChatId, "Выберите маршрут", replyMarkup: keyboard,
+            cancellationToken: cancellationToken);
+        return Unit.Value;
     }
+    
 }
