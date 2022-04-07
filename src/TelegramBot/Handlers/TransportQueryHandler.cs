ca0d832ba70c2edcffd0a7af3042b4bc4a6aaeb4
DimaUraz <102070914+DimaUraz@users.noreply.github.com>
2022-04-07 21:10:40 +0400

    Added Tram and TrolleyBus

--- src/TelegramBot/Handlers/TransportQueryHandler.cs
+++ src/TelegramBot/Handlers/TransportQueryHandler.cs
@@ -13,7 +13,14 @@ public class TransportQueryHandler : IRequestHandler<TransportQuery>
 
     public async Task<Unit> Handle(TransportQuery request, CancellationToken cancellationToken)
     {
-        InlineKeyboardMarkup keyboard = new(AllRoutes().Select(RouteButtons));
+        int[] routes;
+        if (request.Update.UserMessage == "Автобусы")
+            routes = AllBusRoutes();
+        else if (request.Update.UserMessage == "Троллейбусы")
+            routes = AllTrolleyBusRoutes();
+        else
+            routes = AllTramRoutes();
+        InlineKeyboardMarkup keyboard = new(routes.Select(RouteButtons));
         await _telegram.SendTextMessageAsync(request.Update.ChatId, "Выберите маршрут", replyMarkup: keyboard,
             cancellationToken: cancellationToken);
         return Unit.Value;
@@ -27,9 +34,21 @@ public class TransportQueryHandler : IRequestHandler<TransportQuery>
             InlineKeyboardButton.WithCallbackData("Обратное", $"/bus {route} r")
         };
 
-    private int[] AllRoutes() =>
+    private int[] AllBusRoutes() =>
         new[]
         {
             2, 6, 7, 8, 9, 11, 12, 15, 16, 19, 21, 22, 23, 25, 26, 27, 28, 29, 31, 34, 36, 40, 41, 45, 56, 68, 73, 79
+        };
+       
+    private int[] AllTrolleyBusRoutes() =>
+        new[]
+        {
+            1, 2, 4, 6, 7, 9, 10, 14
+        };
+
+    private int[] AllTramRoutes() =>
+        new[]
+        {
+            1, 2, 3, 4, 5, 7, 8, 9, 10, 11, 12
         };
 }
