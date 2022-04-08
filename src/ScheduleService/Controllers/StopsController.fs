6aac0721e7d5e0d9df03be6d080d89f4a8e22475
nikitakaralyus <nikitakaralyus@gmail.com>
2022-04-08 13:48:51 +0400

    Created Dtos

--- src/ScheduleService/Controllers/StopsController.fs
+++ src/ScheduleService/Controllers/StopsController.fs
@@ -1,6 +1,7 @@
 namespace WhereIsTheBus.ScheduleService.Controllers
 
 open Microsoft.AspNetCore.Mvc
+open WhereIsTheBus.ScheduleService.Dtos
 open WhereIsTheBus.ScheduleService.StopsParser
 
 [<ApiController>]
@@ -8,12 +9,12 @@ open WhereIsTheBus.ScheduleService.StopsParser
 type Stops() =
     inherit ControllerBase()
 
-    [<HttpGet>]
+    [<HttpGet("{stopId:int}")>]
     member _.TransportBy(stopId: int) =
         task {
             if stopId > 0 then
                 let! transport = asyncStopTransport stopId
-                return OkObjectResult(transport) :> IActionResult
+                return OkObjectResult(transport |> toTransportDtos) :> IActionResult
             else
                 return NotFoundResult()
         }
--- src/ScheduleService/Dtos.fs
+++ src/ScheduleService/Dtos.fs
@@ -8,6 +8,8 @@ type SharedDirection = WhereIsTheBus.Domain.Enums.Direction
 
 type DomainTransportStop = WhereIsTheBus.Domain.Records.TransportStop
 
+type DomainRoute = WhereIsTheBus.Domain.Records.Route
+
 type TransportRouteDto = {
     Transport: TransportType
     Number: int
@@ -19,6 +21,11 @@ type TransportStopDto = {
     Name: string
     Direction: StrictDirection
     TimeToArrive: int
+}
+
+type TransportDto = {
+    Name: string
+    Routes: DomainRoute seq
 }
 
 let private toInternalTransport transportType =
@@ -40,6 +47,13 @@ let private toDomainDirection direction =
     | Direction.Return -> SharedDirection.Return
     | Direction.Both -> SharedDirection.Both
 
+let private toDomainRoute (route: Route) =
+    let timeToArrive =
+        match route.TimeToArrive with
+        | Minutes m -> m.ToString()
+        | Unspecified s -> s
+    DomainRoute(route.Number, timeToArrive)
+
 let toDto (stop: TransportStop) =
     {
         Id = stop.Id
@@ -47,6 +61,14 @@ let toDto (stop: TransportStop) =
         Direction = (toDomainDirection stop.Direction).AsStrictDirection()
         TimeToArrive = stop.TimeToArrive
     }
+
+let toTransportDtos (transport: seq<Transport>) =
+    transport
+    |> Seq.map(fun x ->
+        {
+            Name = x.Name
+            Routes = x.Routes |> Seq.map toDomainRoute
+        })
 
 type TransportRouteDto with
     member this.Domain() : TransportRoute =
