ffe23467e212ebac390a7406608e135b1310b4a0
nikitakaralyus <nikitakaralyus@gmail.com>
2022-05-22 14:51:27 +0400

    Scaffold project up

---
+++ src/IzhTransport/IzhTransport/IzhTransportApp.swift
@@ -0,0 +1,17 @@
+//
+//  IzhTransportApp.swift
+//  IzhTransport
+//
+//  Created by Nikita Karalius on 22.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct IzhTransportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
